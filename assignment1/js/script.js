7dc6577f5b6d2a2e425902fff3739f6dd19c0cfe
xtalepod <xtalepod@riseup.net>
2020-01-23 22:22:38 -0500

    A1: Updated paint function with comments. Here we created 4 variables: 1 for red, blue, and green respectively which use the Math.random function * 255 to give us our random colours, and a 4th which uses those values to generate the random colours in the CSS pixel style

--- assignment1/js/script.js
+++ assignment1/js/script.js
@@ -15,15 +15,14 @@ window.addEventListener('load', setup);
 //the setup function
 //In this section the div elements are added so that we can
 function setup() {
-//a regular for loop that counts from 0 to 1000 and goes through each pixel
-  for (let i = 0; i < 1000; i ++) {
-    /*add a DOM function to create a new div and store the div in a pixel variable
-    proper syntax note: you need to put div in the brackets here
-    */
+  //a regular for loop that counts from 0 to 1000 and goes through each pixel
+  for (let i = 0; i < 1000; i++) {
+    //add a DOM function to create a new div and store the div in a pixel variable proper syntax note: you need to put div in the brackets here
+
     let newPixel = document.createElement('div');
     /*add a class attribute to newPixel element using the setAttribute() function
     this allows us "to assign the correct" CSS styling*/
-    newPixel.setAttribute('class','pixel');
+    newPixel.setAttribute('class', 'pixel');
     let pixelDiv = document.getElementById('pixel');
     //add the new element to the page's body using the body's appendChild()function
     pixelDiv.appendChild(newPixel);
@@ -36,21 +35,20 @@ function setup() {
 }
 
 
-//define a function called paint with a parameter called 'e'
+//define a function called paint with a parameter called 'e' that deals with colour
 function paint(e) {
   //create a new variable and store e.target (an event handler) in it
   let pixel = e.target;
-
-  let red = (Math.floor(Math.random()*255));
-  let green = (Math.floor(Math.random()*255));
-  let blue = (Math.floor(Math.random()*255));
-
+  //create three variables that use the Math.random() function and multiply them by 255 so that our pixels will be random colours
+  let red = (Math.floor(Math.random() * 255));
+  let green = (Math.floor(Math.random() * 255));
+  let blue = (Math.floor(Math.random() * 255));
+//create a new variable and use a template literal to define its value. we must add rgb to the start of the string because its a CSS requirement
   let colourValue = `rgb(${red},${green},${blue})`;
-//set the pixel background colour to the RGB variable by accessing the pixels CSS style property
+  //set the pixel background colour to the RGB variable by accessing the pixels CSS style property
   pixel.style.backgroundColor = colourValue;
-  //always give a function and a delay
-  //we are telling it WHICH pixel in the last parameter
-  setTimeout(resetPixel,2000, pixel);
+  //a function that calls the resetPixel after a delay of 2000milliseconds and resets the pixel set to 'white'
+  setTimeout(resetPixel, 2000, pixel);
 }
 
 
@@ -59,21 +57,20 @@ function rotate(e) {
 
   if (e.keyCode === 39) {
     rotation += 1;
-  }
-  else if (e.keyCode === 37) {
+  } else if (e.keyCode === 37) {
     rotation -= 1;
   }
 
-//this gives us all of the pixels on the page
-   let pixels = document.getElementsByClassName('pixel');
+  //this gives us all of the pixels on the page
+  let pixels = document.getElementsByClassName('pixel');
 
-   for (let i = 0; i < pixels.length; i ++) {
-     pixels[i].style.transform = `rotate(${rotation}deg)`;
-   }
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].style.transform = `rotate(${rotation}deg)`;
+  }
 }
 
-//resetPixel function is CALLING the pixel variable created in function
-//paint so we need to write it in parenthesis
+//define a function called resetPixel that takes a pixel paramter to revert the colour of the pixel back after mouseover
 function resetPixel(pixel) {
-pixel.style.backgroundColor = 'white';
+  //set the pizel backgroundColor to white by accessing the pixels CSS style property
+  pixel.style.backgroundColor = 'white';
 }
