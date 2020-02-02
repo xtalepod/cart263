4d91c9bafe25d35fd341e98479d58a86a0741ea3
xtalepod <xtalepod@riseup.net>
2020-02-01 21:08:41 -0500

    P1: Working slowly to build an idea... Mostly playing with CSS and HTML still.

--- p1/js/script.js
+++ p1/js/script.js
@@ -9,3 +9,27 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+
+$(document).ready(setup);
+
+function setup () {
+  console.log("setting up");
+
+// https://api.jquery.com/slideToggle/#slideToggle-duration-complete
+  $( "#clickme" ).click(function() {
+    $( "#book" ).slideToggle( "slow", function() {
+      // Animation complete.
+    });
+  });
+
+//https://api.jquery.com/fadeToggle/
+  $( "button" ).first().click(function() {
+  $( "p" ).first().fadeToggle( "slow", "linear" );
+});
+$( "button" ).last().click(function() {
+  $( "p" ).last().fadeToggle( "fast", function() {
+    $( "#log" ).append( "<div>finished</div>" );
+  });
+});
+}
