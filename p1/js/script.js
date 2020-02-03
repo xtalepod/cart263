437ce66b1cd7fd83f9ff44f5e306d307155abf11
xtalepod <xtalepod@riseup.net>
2020-02-03 16:35:56 -0500

    P1: Created scrollbox in HTML, buttons in HTML, CSS, and jQuery with toggle, added gif by sarah zucker + alt message that includes their website, attemping to make a function to rotate the title.

--- p1/js/script.js
+++ p1/js/script.js
@@ -9,19 +9,14 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
-
+//heart, dumpster, gif,
+let limerenceTitleRotation;
 
 $(document).ready(setup);
 
 function setup () {
   console.log("setting up");
 
-// https://api.jquery.com/slideToggle/#slideToggle-duration-complete
-  $( "#clickme" ).click(function() {
-    $( "#book" ).slideToggle( "slow", function() {
-      // Animation complete.
-    });
-  });
 
 //https://api.jquery.com/fadeToggle/
   $( "button" ).first().click(function() {
@@ -29,7 +24,14 @@ function setup () {
 });
 $( "button" ).last().click(function() {
   $( "p" ).last().fadeToggle( "fast", function() {
-    $( "#log" ).append( "<div>finished</div>" );
+    $( "#log" ).append( "<div>no</div>" );
   });
 });
+}
+
+
+function rotateTitle() {
+  limerenceTitleRotation +=1;
+  //select the header to rotate
+  $('#title').text(limerenceTitleRotation);
 }
