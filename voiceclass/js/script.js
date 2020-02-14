b8dc332846e136c6ee7be9829fae84cabd061072
xtalepod <xtalepod@riseup.net>
2020-02-13 19:47:30 -0500

    A3: Double checking my code from class with the example to see where I messed up. Working on the first task from the assignment.

--- voiceclass/js/script.js
+++ voiceclass/js/script.js
@@ -152,6 +152,18 @@ let animals = [
     const NUM_OPTIONS = 5;
     let $correctButton;
 
+//
+  //   let abusePhrases = ["pathetic",
+  // "a loser",
+  // "a waste of space",
+  // "nobody",
+  // "ugly",
+  // "worthless",
+  // "hopeless",
+  // "hideous"];
+//let currentPhrases = '';
+let $command;
+
 $(document).ready(setup);
 
 
@@ -209,7 +221,7 @@ function handleGuess() {
   }
 }
 
-function sayBackwards() {
+function sayBackwards(text) {
   let backwardsText = text.split('').reserve().join('');
   let options = {
     rate: 1,
@@ -218,19 +230,55 @@ function sayBackwards() {
   responsiveVoice.speak("hello world", "UK English Man", options);
 }
 
-function handleUserSpeech(phrase) {
 
-  if (phrase === currentPhrase) {
-    $command.text('Say it again');
+//from pippins game
+function handleAnnyang(){
+  if (annyang) {
+
+    // Add the commands to annyang. That is it should listen
+    // for "I am..." or "I'm..." followed by some number of words.
+    // In annyang's commands an asterisk (*) followed by a
+    // variable names means that annyang will call the function
+    // specified with EVERYTHING it heard from that point on...
+    let command = {
+      "I give up": handleUserSpeech,
+      "I think it is *animal": handleUserSpeech
+    };
+
+    // Now we've defined the commands we give them to annyang
+    // by using its .addCommands() function.
+    annyang.addCommands(command);
+    // Finally we tell annyang to start listening with its
+    // .start() function
+    annyang.start();
+
+    // Choose a phrase for the user to say first
+    currentPhrase = getNewPhrase();
+
+    $command = $('#command');
+    // Display the phrase on the page
+    $command.text(`Say, "I am ${currentPhrase}."`);
   }
-
 }
-// function talk() {
-//     responsiveVoice.speak("hello world", "UK English Man", {
-// //object parameters
-//       pitch: 2,
-//       rate: 2,
-//       volume: 1
-//     });
-//
-// }
+
+function handleUserSpeech(animal) {
+
+  // We check whether the user said what they were told to say
+  // by comparing what annyang heard (phrase) with the
+  // currentPhrase variable
+  if (animal === $correctButton) {
+    // If they said the right thing, we emphasise it...
+    // (Note the use of backslash (\) to "escape" the apostrophe in That's
+    // so that it doesn't interfere with our string.)
+    $command.text(`That's right. You are ${animal}.`);
+    // Get a new thing for them to say
+    currentAnimal = getNewElement();
+    // And tell them to say it
+    $command.append(` Now say "I am ${currentAnimal}."`);
+  }
+  else {
+    // If they said the wrong thing, correct them and demand
+    // they say it.
+    $command.text(`That's not right. Say "I am ${currentAnimal}".`);
+  }
+}
