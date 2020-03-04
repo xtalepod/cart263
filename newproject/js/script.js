dcb311954ed1abecf23450bf989c07c9ab62ffd4
xtalepod <xtalepod@riseup.net>
2020-03-03 21:39:47 -0500

    P2: Working on a "start screen" using My Digital Tears P1 as a reference (cause its great!) https://mydigitaltears.github.io/cart263/Project1/. Currently included setInternal( ), startButtonPressed details, background flash. Moved trap actions from setup to startButtonPressed. Updated HTML and CSS accordingly.

--- newproject/js/script.js
+++ newproject/js/script.js
@@ -28,6 +28,13 @@ let count = 0;
 let $trap;
 //the array for the trapezoids
 let trapazoids = [];
+//html elements adapted from my digital tears
+let body;
+let firstForm;
+let start;
+//internvals adapted from my digital tears
+let startInterval;
+
 
 $(document).ready(setup);
 
@@ -36,42 +43,37 @@ function setup() {
 
   // $trap = $('#trapezoid');
   $trap = $('#container');
+  //adapted from my digital tears
+  start = $(".start");
+  firstForm = $(".firstForm");
+   body = $("body");
 
   //https://jqueryui.com/dialog/#modal-message
-  $( function() {
-      $( "#dialog-confirm" ).dialog({
-        resizable: false,
-        height: "auto",
-        width: 400,
-        modal: true,
-        buttons: {
-          "i accept": function() {
-            // $(this ).dialog( "tyes" );
-          },
-          //i dont accept
-          // Cancel: function() {
-          //   $( this ).dialog( "close" );
-          // }
-        }
-      });
-    } );
+  // $( function() {
+  //      $( "#dialog-confirm" ).dialog({
+  //        resizable: false,
+  //        height: "auto",
+  //        width: 400,
+  //        modal: true,
+  //        buttons: {
+  //          "i accept": function() {
+  //            $(this ).dialog( "close" );
+  //          },
+  //          //i dont accept
+  //          // Cancel: function() {
+  //          //   $( this ).dialog( "close" );
+  //          // }
+  //        }
+  //      });
+  //    } );
 
-     //this is the same dialog box with responsive voice
-     $( "#dialog-confirm" ).on('click',function () {
-   // say() is a function defined below
-     say("In this moment of social crisis, where even the most basic assertion that black lives matter is contested, we are drowning in “the facts” of inequality and injustice. Whether it is a new study on criminal justice disparities or another video of police brutality, demanding empirical evidence of systematic wrongdoing can have a kind of perverse quality—as if subjugated people must petition again and again for admission into the category of “human,” for which empathy is rationed andapplications are routinely denied. Ruha Benjamin");
- });
-
-
-console.log($("dialog-confirm").on);
+ //     //this is the same dialog box with responsive voice
+ //     $( "#dialog-confirm" ).on('click',function () {
+ //   // say() is a function defined below
+ //     say("In this moment of social crisis, where even the most basic assertion that black lives matter is contested, we are drowning in “the facts” of inequality and injustice. Whether it is a new study on criminal justice disparities or another video of police brutality, demanding empirical evidence of systematic wrongdoing can have a kind of perverse quality—as if subjugated people must petition again and again for admission into the category of “human,” for which empathy is rationed andapplications are routinely denied. Ruha Benjamin");
+ // });
 
   for (let i = 0; i < 5; i++) {
-   // $trap.hide()
-   //   .fadeIn(300)
-   //   .mouseenter(function() {
-   //     $("p").first().text("enter");
-   //     $("p").last().text(++count);
-   //      say("ahhhh");
          let x = Math.floor(Math.random() * 100);
          let y = Math.floor(Math.random() * 500 + 100);
          trapazoids.push(new Trap (x, y, 20, '#f62681'));
@@ -80,7 +82,46 @@ console.log($("dialog-confirm").on);
     // trapActions();
 
     }
+
+    // interval to make a flashy random color background from my digital tears
+startInterval = setInterval(backgroundFlash, 100);
 }
+
+// random color background from my digital tears
+function backgroundFlash() {
+  let r = Math.random()*255;
+  let g = Math.random()*0;
+  let b = Math.random()*199;
+  body.css("background-color", `rgb(${r},${g},${b})`)
+}
+
+function startButtonPressed() {
+ // // clear the flashy background interval
+ clearInterval(startInterval);
+   trapActions();
+ // sets the background to the vaporwave picture
+ body.css("background-image",'url("./assets/images/chkpattern.jpg")');
+ // hide the div from the start
+ start.css("display", "none");
+ // show the first form and the counter
+ firstForm.css("display", "block");
+ // counter.css("display", "block");
+ // incrementCount();
+ // modal dialog box
+ $( "#form1" ).dialog({
+   dialogClass: "no-close",
+   modal: "true",
+   buttons: [
+     {
+       text: "OK",
+       click: function() {
+         $( this ).dialog( "close" );
+       }
+     }
+   ]
+ });
+}
+
 
 function say(text) {
   responsiveVoice.speak(text, voice, voiceParameters);
