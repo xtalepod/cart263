33cd5d28e27974485a2f879f8e9c68b9098781cb
xtalepod <xtalepod@riseup.net>
2020-03-04 15:30:48 -0500

    P2: Removed dialog boxes and making a basic "start" page

--- newproject/js/script.js
+++ newproject/js/script.js
@@ -26,6 +26,7 @@ let voiceParameters = {
 let count = 0;
 //the jQuery object for the trapezoid element
 let $trap;
+let $button;
 //the array for the trapezoids
 let trapazoids = [];
 //html elements adapted from my digital tears
@@ -41,51 +42,55 @@ $(document).ready(setup);
 
 function setup() {
 
-  // $trap = $('#trapezoid');
   $trap = $('#container');
+  $button = $('button');
   //adapted from my digital tears
   start = $(".start");
   firstForm = $(".firstForm");
-   body = $("body");
+  body = $("body");
 
   //https://jqueryui.com/dialog/#modal-message
-  // $( function() {
-  //      $( "#dialog-confirm" ).dialog({
-  //        resizable: false,
-  //        height: "auto",
-  //        width: 400,
-  //        modal: true,
-  //        buttons: {
-  //          "i accept": function() {
-  //            $(this ).dialog( "close" );
-  //          },
-  //          //i dont accept
-  //          // Cancel: function() {
-  //          //   $( this ).dialog( "close" );
-  //          // }
-  //        }
-  //      });
-  //    } );
+  // $(function() {
+  //   $("#dialog-confirm").dialog({
+  //     resizable: false,
+  //     height: "auto",
+  //     width: 400,
+  //     modal: true,
+  //     buttons: {
+  //       "i accept": function() {
+  //         $(this).dialog("close");
+  //       },
+  //       //i dont accept
+  //       // Cancel: function() {
+  //       //   $( this ).dialog( "close" );
+  //       // }
+  //     }
+  //   });
+  // });
 
- //     //this is the same dialog box with responsive voice
- //     $( "#dialog-confirm" ).on('click',function () {
- //   // say() is a function defined below
- //     say("In this moment of social crisis, where even the most basic assertion that black lives matter is contested, we are drowning in “the facts” of inequality and injustice. Whether it is a new study on criminal justice disparities or another video of police brutality, demanding empirical evidence of systematic wrongdoing can have a kind of perverse quality—as if subjugated people must petition again and again for admission into the category of “human,” for which empathy is rationed andapplications are routinely denied. Ruha Benjamin");
- // });
+  // $("#dialog-confirm").on('click', function() {
+  //   //   // say() is a function defined below
+  //   say("In this moment of social crisis, where even the most basic assertion that black lives matter is contested, we are drowning in “the facts” of inequality and injustice. Whether it is a new study on criminal justice disparities or another video of police brutality, demanding empirical evidence of systematic wrongdoing can have a kind of perverse quality—as if subjugated people must petition again and again for admission into the category of “human,” for which empathy is rationed andapplications are routinely denied. Ruha Benjamin");
+  // });
+
+  //     //this is calling the html start div
+   start.on('click',function () {
+    // say() is a function defined below
+      say("In this moment of social crisis, where even the most basic assertion that black lives matter is contested, we are drowning in “the facts” of inequality and injustice. Whether it is a new study on criminal justice disparities or another video of police brutality, demanding empirical evidence of systematic wrongdoing can have a kind of perverse quality—as if subjugated people must petition again and again for admission into the category of “human,” for which empathy is rationed andapplications are routinely denied. Ruha Benjamin");
+  });
 
   for (let i = 0; i < 5; i++) {
-         let x = Math.floor(Math.random() * 100);
-         let y = Math.floor(Math.random() * 500 + 100);
-         trapazoids.push(new Trap (x, y, 20, '#f62681'));
+    let x = Math.floor(Math.random() * 1000);
+    let y = Math.floor(Math.random() * 500 + 100);
+    trapazoids.push(new Trap(x, y, 20, '#f62681'));
     //  });
-  // }
+    }
     // trapActions();
 
-    }
+      // interval to make a flashy random color background from my digital tears
+  startInterval = setInterval(backgroundFlash, 100);
+  }
 
-    // interval to make a flashy random color background from my digital tears
-startInterval = setInterval(backgroundFlash, 100);
-}
 
 // random color background from my digital tears
 function backgroundFlash() {
@@ -96,30 +101,29 @@ function backgroundFlash() {
 }
 
 function startButtonPressed() {
- // // clear the flashy background interval
- clearInterval(startInterval);
-   trapActions();
- // sets the background to the vaporwave picture
- body.css("background-image",'url("./assets/images/chkpattern.jpg")');
- // hide the div from the start
- start.css("display", "none");
- // show the first form and the counter
- firstForm.css("display", "block");
- // counter.css("display", "block");
- // incrementCount();
- // modal dialog box
- $( "#form1" ).dialog({
-   dialogClass: "no-close",
-   modal: "true",
-   buttons: [
-     {
-       text: "OK",
-       click: function() {
-         $( this ).dialog( "close" );
-       }
-     }
-   ]
- });
+  // // clear the flashy background interval
+  clearInterval(startInterval);
+  trapActions();
+  // sets the background to the vaporwave picture
+  body.css("background-image", 'url("./assets/images/chkpattern.jpg")');
+  // hide the div from the start
+  start.css("display", "none");
+  // show the first form and the counter
+  firstForm.css("display", "block");
+
+  // counter.css("display", "block");
+  // incrementCount();
+  // modal dialog box
+  // $("#form1").dialog({
+  //   dialogClass: "no-close",
+  //   modal: "true",
+  //   buttons: [{
+  //     text: "OK",
+  //     click: function() {
+  //       $(this).dialog("close");
+  //     }
+  //   }]
+  // });
 }
 
 
@@ -129,13 +133,13 @@ function say(text) {
 
 function trapActions() {
 
-for (let i = 0; i < trapazoids.length; i++) {
+  for (let i = 0; i < trapazoids.length; i++) {
     $trap.hide()
-    .fadeIn(300)
-    .mouseenter(function() {
-      $("p").first().text("mouse enter");
-      $("p").last().text(++count);
-      say("ahhhh");
-    });
+      .fadeIn(300)
+      .mouseenter(function() {
+        $("p").first().text("mouse enter");
+        $("p").last().text(++count);
+        say("ahhhh");
+      });
   }
 }
