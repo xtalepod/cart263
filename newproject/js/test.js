f010874b6072bef2ce9a59a5efbf8dbd85f1b2be
xtalepod <xtalepod@riseup.net>
2020-03-05 11:48:14 -0500

    P2: Created a new function "speakingParameters" with random values for pitch, volume, and rate so that its less normal when the user overlaps an image.

--- newproject/js/test.js
+++ newproject/js/test.js
@@ -4,11 +4,12 @@
 let voice = 'UK English Male';
 
 // The parameters for the voice in an object
-let voiceParameters = {
-  pitch: 0.6,
-  rate: 0.6,
-  volume: 1
-}
+// let voiceParameters  = {
+//      pitch: 2,
+//      rate: 1,
+//      volume: 5
+//    };
+
 $(document).ready(setup);
 //
 //
@@ -17,24 +18,39 @@ function setup() {
 
 
 // https://stackoverflow.com/questions/34767900/jquery-replace-image-on-hover/34768036#34768036
+//this calls the image container class from the HTML file and makes it so that each img src in that class changes (respectively) when the mouse hovers over
   $(".image-container").mouseover(function () {
   $(this).attr('src', $(this).data("hover"));
 }).mouseout(function () {
   $(this).attr('src', $(this).data("src"));
 });
 
+//these call the HTML id's of my two image-container class img src objects and use the say function to make them say their own things
   $("#raffy").mouseenter(function() {
    // say() is a function defined below
-     say("In this moment of social crisis, where even the most basic assertion that black lives matter is contested, we are drowning in “the facts” of inequality and injustice. Whether it is a new study on criminal justice disparities or another video of police brutality, demanding empirical evidence of systematic wrongdoing can have a kind of perverse quality—as if subjugated people must petition again and again for admission into the category of “human,” for which empathy is rationed andapplications are routinely denied. Ruha Benjamin");
+     speakingParameters("In this moment of social crisis, where even the most basic assertion that black lives matter is contested, we are drowning in “the facts” of inequality and injustice. Whether it is a new study on criminal justice disparities or another video of police brutality, demanding empirical evidence of systematic wrongdoing can have a kind of perverse quality—as if subjugated people must petition again and again for admission into the category of “human,” for which empathy is rationed andapplications are routinely denied. Ruha Benjamin");
  });
-
  $("#4chan").mouseenter(function() {
   // say() is a function defined below
-    say("okay");
+    speakingParameters("okay");
 });
 
 }
 
-function say(text) {
+// function say(text) {
+//   responsiveVoice.speak(text, voice, voiceParameters);
+// }
+
+function speakingParameters(text) {
+    let randomPitch = Math.random();
+    let randomVolume = Math.random();
+    let randomRate = Math.random();
+
+  let voiceParameters  = {
+     pitch: randomPitch,
+     rate: randomRate,
+     volume: 5
+   };
+
   responsiveVoice.speak(text, voice, voiceParameters);
 }
