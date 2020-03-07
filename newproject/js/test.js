6da364bf46bbb782615086660ea5469743706600
xtalepod <xtalepod@riseup.net>
2020-03-07 13:29:02 -0500

    P2: Now when you click the button the image flips but only one way... Created array for images and image srcs.

--- newproject/js/test.js
+++ newproject/js/test.js
@@ -1,63 +1,46 @@
 "use strict";
-//
-
-let voice = 'UK English Male';
-
-let countP = 0;
-let $counter;
-// let countE;
-let body;
-let question;
-
-let dialogBoxes = [
-  "eros",
-  "i think theres something missing inside me",
-  "the state of being infatuated or obsessed with another person",
-  "typically experienced involuntarily and characterized by a strong desire for reciprocation",
-  "i am self assured",
-  "have you read the wiki page 'how to let go?'"
-];
 
 let $raffy;
 let $fourchan;
 let $democracy;
 let imagesArray = [];
-
-
-
+console.log($democracy);
 
 $(document).ready(setup);
 //
 //
 function setup() {
 
-$counter = $("#counter");
+
+// $counter = $("#counter");
 $raffy= $("#raffy");
 $fourchan =$("#fourchan");
 $democracy = $("#democracy");
 
+ $("#democracy").attr('src','assets/images/democracy.png');
 
-}
-
-function imageFlip(){
-  imageArray = [$("#raffy"),$("#fourchan"),$("#democracy")];
-
-  for (let i = 0; i < 15 ; i++) {
-    let x = Math.floor(Math.random() * 1000);
-    let y = Math.floor(Math.random() * 500 + 100);
-    imagesArray.push();
+  $('#democracy').on({
+    'click': function() {
+         var src = ($(this).attr('src') === 'assets/images/4chan.png')
+            ? 'assets/images/democracynow.png'
+            : 'assets/images/4chan.png';
+         $(this).attr('src', src);
     }
-  console.log(imagesArray);
-  console.log(imageFlip);
+});
+
 }
 
-// //a function to deal with what happens when the user clicks this button
-function perversionButtonPressed() {
-  // body.css("background-image", 'url("./assets/images/chkpattern.jpg")');
-  addDialog();
-  // addImage();
-  $("#counter").text(countP++);
-  }
+// function imageFlip(){
+//   imageArray = [$("#raffy"),$("#fourchan"),$("#democracy")];
+//
+//   for (let i = 0; i < 15 ; i++) {
+//     let x = Math.floor(Math.random() * 1000);
+//     let y = Math.floor(Math.random() * 500 + 100);
+//     imagesArray.push();
+//     }
+//   console.log(imagesArray);
+//   console.log(imageFlip);
+// }
 
 //i dont want to ADD an image, i want an image array already on the page that flips an image when you click the perv button
 //and i want a dialog to accompany a picture flipping
@@ -85,54 +68,54 @@ function perversionButtonPressed() {
 //     speakingParameters("okay");
 // });
 // }
-
-function addDialog() {
-  // Dynamically create a div and store it in a variable. This is the div
-  // we will turn into a dialog box. Set its title at the same time.
-  let $dialog = $(`<div></div>`).attr(`title`, `spiral`);
-  $dialog.hide();
-  // Choose a random question text from the array
-  question = dialogBoxes[Math.floor(randomInRange(0, dialogBoxes.length))];
-  // Add a p tag to the dialog div that contains the question text
-  $dialog.append(`<p>${question}</p>`);
-  // Finally, add the div to the page
-  $('body').append($dialog);
-
-  // Now we have our div on the page, transform it into a dialog with jQuery UI's
-  // .dialog() method, supplying a number of options to configure it
-  $dialog.dialog({
-    //from endless dialog
-    // buttons: {
-    //   "ask me again": function() {
-    //       addDialog();
-    //   },
-    // },
-    // The 'containment' option lets us specify where the dialog can go on the screen. 'body' means it will be
-    // contained within the body tag, and can't be dragged out of it.
-    containment: 'body'
-  });
-
-  // Finally, use .offset() on the .parent() of the dialog in order to give it a random position on the screen.
-  // Uses .height() and .width() to get the dimensions of elements, including the window.
-  $dialog.parent().offset({
-    top: Math.random() * ($(window).height() - $dialog.parent().height()),
-    left: Math.random() * ($(window).width() - $dialog.parent().width())
-  });
-}
-// Returns a random number between min and max
-function randomInRange(min, max) {
-  return min + (Math.random() * (max - min));
-}
-function speakingParameters(text) {
-    let randomPitch = Math.random();
-    let randomVolume = Math.random();
-    let randomRate = Math.random();
-
-  let voiceParameters  = {
-     pitch: randomPitch,
-     rate: randomRate,
-     volume: 5
-   };
-
-  responsiveVoice.speak(text, voice, voiceParameters);
-}
+//
+// function addDialog() {
+//   // Dynamically create a div and store it in a variable. This is the div
+//   // we will turn into a dialog box. Set its title at the same time.
+//   let $dialog = $(`<div></div>`).attr(`title`, `spiral`);
+//   $dialog.hide();
+//   // Choose a random question text from the array
+//   question = dialogBoxes[Math.floor(randomInRange(0, dialogBoxes.length))];
+//   // Add a p tag to the dialog div that contains the question text
+//   $dialog.append(`<p>${question}</p>`);
+//   // Finally, add the div to the page
+//   $('body').append($dialog);
+//
+//   // Now we have our div on the page, transform it into a dialog with jQuery UI's
+//   // .dialog() method, supplying a number of options to configure it
+//   $dialog.dialog({
+//     //from endless dialog
+//     // buttons: {
+//     //   "ask me again": function() {
+//     //       addDialog();
+//     //   },
+//     // },
+//     // The 'containment' option lets us specify where the dialog can go on the screen. 'body' means it will be
+//     // contained within the body tag, and can't be dragged out of it.
+//     containment: 'body'
+//   });
+//
+//   // Finally, use .offset() on the .parent() of the dialog in order to give it a random position on the screen.
+//   // Uses .height() and .width() to get the dimensions of elements, including the window.
+//   $dialog.parent().offset({
+//     top: Math.random() * ($(window).height() - $dialog.parent().height()),
+//     left: Math.random() * ($(window).width() - $dialog.parent().width())
+//   });
+// }
+// // Returns a random number between min and max
+// function randomInRange(min, max) {
+//   return min + (Math.random() * (max - min));
+// }
+// function speakingParameters(text) {
+//     let randomPitch = Math.random();
+//     let randomVolume = Math.random();
+//     let randomRate = Math.random();
+//
+//   let voiceParameters  = {
+//      pitch: randomPitch,
+//      rate: randomRate,
+//      volume: 5
+//    };
+//
+//   responsiveVoice.speak(text, voice, voiceParameters);
+// }
