d7c76932c477c55d6cb1869fee2cbaafb0721fa1
xtalepod <xtalepod@riseup.net>
2020-03-26 21:02:52 -0400

    A4: Did this assignment last week but didn't have github desktop set up or anything so commits weren't working (borrowed laptop life). Going to try to add commits now and remember the steps I took...

--- Assignments/Assignment4/js/script.js
+++ Assignments/Assignment4/js/script.js
@@ -9,7 +9,7 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
-
+nsgjsgh
 
 $(document).ready(setup);
 
