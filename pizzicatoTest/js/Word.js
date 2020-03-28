493dfd390977a229016d55bd7fe438c33244ccce
xtalepod <xtalepod@riseup.net>
2020-03-28 16:57:06 -0400

    p3: its working

--- pizzicatoTest/js/Word.js
+++ pizzicatoTest/js/Word.js
@@ -1,15 +1,13 @@
 "use strict";
 
 class Word {
-
   constructor(wordText, x, y, color, sound) {
     this.wordText = wordText;
     this.x = x;
     this.y = y;
     this.color = color;
     this.sound = sound;
-    // this.sound.play();
-    console.log(this.sound); // undefined
+    // console.log(this.sound); // undefined
     this.element = $('<div>');
     $(this.element).text(this.wordText);
     $(this.element).css("color", this.color);
@@ -17,44 +15,5 @@ class Word {
     $(this.element).css("position", "absolute");
     $(this.element).css("left", this.x);
     $(this.element).css("top", this.y);
-
-
   }
-
-  clickReaction() {
-    //https://stackoverflow.com/questions/3273350/jquerys-click-pass-parameters-to-user-function
-    $(this.element).click({sound: this.sound, test: "test"}, function(e) {
-      console.log("CLIked");
-      console.log(e.data.test);
-      console.log(e.data.sound);
-      e.data.sound.play();
-          // parameters.play();
-          // sound.play();
-          // console.log(parameters.pla)
-      //inspo i has a deatached object https://en.wikipedia.org/wiki/Detached_object
-    });
-
-    $(this.element).hover({sound: this.sound, test: "test"}, function(e) {
-      console.log("hover");
-      console.log(e.data.test);
-      console.log(e.data.sound);
-      e.data.sound.play();
-      // sleep(polymorphism.duration).then(() => { //this comes from the sleep function source
-      //   polymorphism.stop();
-        // parameters.stop();
-      // });
-    }, function() {
-      // polymorphism.stop();
-      // synth.stop();
-      // polymorphism.showMood();//console log in the MySound class function pings to here
-      // console.log(bark.mood);
-    });
-
-  }
-
-  // https://www.sitepoint.com/delay-sleep-pause-wait/
-  // sleep(ms) {
-  // return new Promise(resolve => setTimeout(resolve, ms));
-  // }
-  //
 } //end of script
--- pizzicatoTest/js/test.js
+++ pizzicatoTest/js/test.js
@@ -3,7 +3,7 @@
 // let synth;
 //my test sounds
 let sound1, sound2, sound3, sound4, sound5;
-
+let mySoundsArray = [];
 
 let myWord
 let numOfWords = 5;
@@ -14,13 +14,8 @@ let aMyString = [
   'let',
   'this'
 ];
-// let aMyString = [
-//   'polymorphism',
-//   'parameters',
-// ];
 
 let myWordsArray = [];
-let mySoundsArray = [];
 
 $(document).ready(setup);
 
@@ -42,47 +37,66 @@ function setup() {
   sound3 = new MySound('variable', "dark", 2000);
   sound4 = new MySound('let', "dark", 2000);
   sound5 = new MySound('this', "dark", 2000);
-
   mySoundsArray.push(sound1);
   mySoundsArray.push(sound2);
   mySoundsArray.push(sound3);
   mySoundsArray.push(sound4);
   mySoundsArray.push(sound5);
-
   // sound1.play(); //putting this here this breaks pizzicato
-
   initWords();
-
-  // variable = new MySound('variable',"dark", 2000);
-  // forSound = new MySound('for',"dark", 2000);
-  // letSound = new MySound('let',"dark", 2000);
-  // thisSound = new MySound('this',"dark", 4000);
-
 
 
 } //endsetup
 
 function initWords(){
-  for (let i = 0; i < numOfWords; i++) {
+  for (let i = 0; i < aMyString.length; i++) {
     let x = Math.random() * 200;
-    let y = Math.random() * 150;
+    let y = Math.random() * 50;
     // let wordText = aMyString[Math.floor(randomInRange(0, aMyString.length))];
     let wordText = aMyString[i];
     // myWord = new Word(wordText, x, y, '#00FF00', parameters)
     // myWord.clickReaction();
-
     myWordsArray.push(new Word(wordText, x, y, '#00FF00', mySoundsArray[i]));
     // myWordsArray.push(new Word(wordText, x, y, '#00FF00', sound1));
-
-
   }
 
-  for (var j = 0; j < myWordsArray.length; j++) {
-    myWordsArray[j].clickReaction();
+  for (let j = 0; j < myWordsArray.length; j++) {
+  $(myWordsArray[j].element).click(function(){
+      console.log("clicked", j);
+      // sound1.play();
+    });
   }
-
-
 }
+
+// function clickReaction() {
+
+  // myWordArray
+//   //https://stackoverflow.com/questions/3273350/jquerys-click-pass-parameters-to-user-function
+  // $().click({
+        // parameters.play();
+        // console.log(parameters.pla)
+    //inspo i has a deatached object https://en.wikipedia.org/wiki/Detached_object
+  // });
+//
+//   $(this.element).hover({
+//     event.data.sound.play();
+//     // sleep(polymorphism.duration).then(() => { //this comes from the sleep function source
+//     //   polymorphism.stop();
+//     // });
+//   }, function() {
+//     // polymorphism.stop();
+//     // synth.stop();
+//     // polymorphism.showMood();//console log in the MySound class function pings to here
+//     // console.log(bark.mood);
+//   });
+//
+// }
+//
+// // https://www.sitepoint.com/delay-sleep-pause-wait/
+// function sleep(ms) {
+// return new Promise(resolve => setTimeout(resolve, ms));
+// }
+// //
 // //from class
 function randomInRange(min, max) {
   return min + (Math.random() * (max - min));
